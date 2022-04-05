07b4e7fb81085a9f106269281f7d55119617bfc8
Ly Ly <lyly@example.com>
2022-04-04 23:17:46 -0700

    display lissajous on the web browser at port 8000

--- chapter_1_tutorial/server1.go
+++ chapter_1_tutorial/server1.go
@@ -2,7 +2,13 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
 	"log"
+	"math"
+	"math/rand"
 	"net/http"
 )
 
@@ -13,5 +19,41 @@ func main() {
 
 //handler echoes the path component of the requested url
 func handler(w http.ResponseWriter, r *http.Request) {
+	lissajous(w)
 	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
+
+var palette = []color.Color{color.White, color.Black}
+
+const (
+	whiteIndex = 0 // first color in palette
+	blackIndex = 1 // next color in palette
+)
+
+func lissajous(out io.Writer) {
+	const (
+		cycles  = 5
+		res     = 0.001
+		size    = 100
+		nframes = 64
+		delay   = 8
+	)
+
+	freq := rand.Float64() * 3.0 // relative freq of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
 }
