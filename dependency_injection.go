067119ddbb9532b2631ad2af08d61f9c616efcab
Lynguyen237 <lynguyen237@gmail.com>
2022-04-21 23:43:51 -0700

    Create dependency injection example

---
+++ dependency_injection.go
@@ -0,0 +1,34 @@
+// interface - defines the methods the consumers should expect
+type SQSHandler interface {
+	func handle(msg interface{}) error
+}
+
+
+// implementation - details of the implementation should be transparent to consumers
+type CostaSQSHandler struct {
+	// dependencies - should be injected (i.e passed in) - allows for easier mocking/testing
+	messagingClient messaging.Client
+	emailClient email.IEmailClient
+}
+
+func (h *messagingSQSHandler) handle(msg interface{}) error {
+	// Do costa message parsing
+	json.ParseStuff(msg)
+
+	// Call costa API to get report details
+	h.messagingClient.GetReport(...)
+	
+	// Send email to issuer
+	h.emailClient.SendEmail(...)
+}
+
+// factory function - instantiates the implementation, but exposes (returns) the interface
+// 
+// Returning the interface ensures that the downstream consumers (i.e. the rest of the application)
+// only know about the interface and do not know about the implementation. This promotes loose coupling.
+func NewMessagingSQSHandler(messagingClient messaging.Client, emailClient email.IEmailClient) SQSHandler {
+	return &MessagingSQSHandler{
+		messagingClient: messagingClient,
+		emailClient: emailClient,
+	}
+}
